a23839c5e60b9379284ce66fa4e0e571514a5aae
José Gabriel Vázquez Gil <jgaby1997@gmail.com>
2023-12-21 19:48:34 +0100

    Init commit

---
+++ src/Entity/UsuarioDTO.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioDTORepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsuarioDTORepository::class)]
+class UsuarioDTO
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $apellido = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fechaNacimiento = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): static
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?string
+    {
+        return $this->fechaNacimiento;
+    }
+
+    public function setFechaNacimiento(string $fechaNacimiento): static
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+
+        return $this;
+    }
+}
